1e16687abb3cf69daaaf2bd14e4f1dd4e5c54b2d
Eduardo Oliveira <pro.eduardo@hotmail.com>
2022-10-17 00:22:18 -0300

    feat/new menu

--- application/manage.py
+++ application/manage.py
@@ -6,5 +6,17 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route("/contrato")
+def contrato():
+    return render_template('page/contrato.html',menu='master', submenu='contrato')
+
+@app.route("/veiculo")
+def veiculo():
+    return render_template('page/veiculo.html',menu='master', submenu='veiculo')
+
+@app.route("/documentos")
+def documentos():
+    return render_template('page/documentos.html',menu='master', submenu='documentos')
+
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=5000)
